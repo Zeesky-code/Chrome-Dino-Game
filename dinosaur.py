7c3ad9f713c6c234a205c6dd02e3dc12ae203e57
Zainab Lawal <zainablaw1012@gmail.com>
2021-09-10 20:13:00 +0300

    Defined the jump function and implemented it

--- dinosaur.py
+++ dinosaur.py
@@ -12,4 +12,13 @@ class Dinosaur:
         self.width = DINOWIDTH
         self.surfaceHeight = surfaceHeight
     def jump(self):
-        
+        if(self.y == 0):   #so the dino oly jumps when it's on the ground
+            self.yvelocity = 300
+    def update(self,deltaTime):
+        self.yvelocity += -500*deltaTime   #gravity
+        self.y +=self.yvelocity*deltaTime
+        if self.y < 0:
+            self.y = 0
+            self.yvelocity =0
+    def draw(self,display):
+        pygame.draw.rect(display,dinocolour,[self.x,self.surfaceHeight-self.y-self.height,self.width,self.height])
--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import pygame
+from dinosaur import Dinosaur
 pygame.init()
 
 size = width,height = 640,480
 gameDisplay = pygame.display.set_mode(size)
+pygame.display.set_caption('Chrome Dino')
 GROUND_HEIGHT = height -100
 
 black = 0,0,0
@@ -10,14 +12,26 @@ white = 255,255,255
 xPos = 0
 yPos = 0
 
+dinosaur = Dinosaur(GROUND_HEIGHT)
+lastFrame = pygame.time.get_ticks()
+
 while True:
+    t = pygame.time.get_ticks()
+    deltaTime = (t-lastFrame)/1000.0
+    lastFrame = t
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             quit()
-    gameDisplay.fill(black)
-    pygame.draw.rect(gameDisplay, white, [0, GROUNDHEIGHT, width, height-GROUND_HEIGHT])
-    xPos += 1
-    yPos +=1
 
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                dinosaur.jump()
+
+    gameDisplay.fill(black)
+
+    dinosaur.update(deltaTime)
+    dinosaur.draw(gameDisplay)
+    
+    pygame.draw.rect(gameDisplay, white, [0, GROUND_HEIGHT, width, height-GROUND_HEIGHT])
     pygame.display.update()
